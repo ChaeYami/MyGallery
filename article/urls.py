7648605723736def1bff418708ef9c617ceb1ef0
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-30 15:05:20 +0900

    init

--- article/urls.py
+++ article/urls.py
@@ -1,79 +1,33 @@
-from rest_framework import serializers
-from .models import Article, Comment
+from django.urls import path
+from article import views
+
+app_name= 'articles'# 테스트 코드
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-    uploaded_image = serializers.ImageField()
-    changed_image = serializers.ImageField()
-    created_at = serializers.DateTimeField(
-        format="%Y-%m-%d %H:%M", read_only=True
-    )
-    # id = serializers.IntegerField()  # 'id' 필드 추가(테스트 코드)
-    def get_user(self, obj):
-        return {"nickname": obj.user.nickname, "pk": obj.user.pk,"profile_img": str(obj.user.profile_img)} # 닉네임 사용해야 함
-        # return obj.user.id
-    class Meta:
-        model = Article
-        fields = "__all__"
-
-# 테스트 코드 에러 주석 처리
-class ArticleCreateSerializer(serializers.ModelSerializer):
-    uploaded_image = serializers.ImageField()
-    changed_image = serializers.ImageField()
-
-    class Meta:
-        model = Article
-        fields = ["title", "content", "uploaded_image", "changed_image", "change_id"]
-        extra_kwargs={
-            "title": {
-                "error_messages": {
-                    "blank": "제목을 입력해주세요",
-                }
-            },
-            "content": {
-                "error_messages": {
-                    "blank": "내용을 입력해주세요",
-                },
-            },
-            "uploaded_image": {
-                "error_messages": {
-                    "blank": "사진 있어야 됨",
-                },
-            },
-        }
-        
 
 
-class ArticleListSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-    changed_image = serializers.ImageField()
+urlpatterns = [
+    path("", views.ArticleView.as_view(), name="article_view"),
+    path(
+        "<int:article_id>/",
+        views.ArticleDetailView.as_view(),
+        name="article_detail_view",
+    ),
+    path(
+        "<int:article_id>/hearts/",
+        views.ArticleHeartsView.as_view(),
+        name="Hearts_view",
+    ),  # 좋아요 기능
+    path(
+        "hearts/<int:user_id>/", views.HeartsListView.as_view(), name="User_Hearts_View"
+    ),  # 좋아요 한 게시글
+    path("<int:article_id>/comment/", views.CommentView.as_view(), name="comment_view"),
+    path(
+        "comment/<int:comment_id>/", views.CommentView.as_view(), name="comment_delete"
+    ),
 
-    def get_user(self, obj):
-        return {"nickname": obj.user.nickname, "id": obj.user.id, "profile_img": str(obj.user.profile_img)}
-
-    class Meta:
-        model = Article
-        fields = ["id", "title", "user", "changed_image", "hearts"]
-        
-        
-
-class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-    comment_created_at = serializers.DateTimeField(
-        format="%y-%m-%d %H:%M", read_only=True
-    )
-
-    def get_user(self, obj):
-        return {"nickname": obj.user.nickname, "pk": obj.user.pk,"profile_img": str(obj.user.profile_img)}
-
-    class Meta:
-        model = Comment
-        exclude = ("article",)  # 게시글 필드 빼고 보여주기
-
-
-# comments작성
-class CommentCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ("comment",)
+    path(
+        "list/<int:user_id>/", views.ArticleListView.as_view(), name="article_list"
+    ),  # /article/<int:article_id>/list
+     path('hearts_rank/', views.HeartsListView.as_view(), name='hearts_rank'),
+]
